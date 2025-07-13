10bcc9804f9445a07e4b6e9333b3106112fac3bc
Benz <chengdevith5@gmail.com>
2025-07-13 13:34:48 +0700

    add DTO validation each controller

--- src/main/java/co/istad/mobilebanking/controller/AccountController.java
+++ src/main/java/co/istad/mobilebanking/controller/AccountController.java
@@ -6,6 +6,7 @@ import co.istad.mobilebanking.dto.CreateAccountRequest;
 import co.istad.mobilebanking.dto.DisableAccountRequest;
 import co.istad.mobilebanking.dto.UpdateAccountRequest;
 import co.istad.mobilebanking.service.AccountService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -46,12 +47,14 @@ public class AccountController {
     }
 
     @PatchMapping("/{actNo}")
-    public AccountResponse updateByActNo(@PathVariable String actNo, @RequestBody UpdateAccountRequest updateAccountRequest) {
+    public AccountResponse updateByActNo(@PathVariable String actNo,
+                                         @Valid @RequestBody UpdateAccountRequest updateAccountRequest) {
         return accountService.updateByActNo(actNo, updateAccountRequest);
     }
 
     @PutMapping("/{actNo}")
-    public AccountResponse disableByActNo(@PathVariable String actNo, @RequestBody DisableAccountRequest disableAccountRequest) {
+    public AccountResponse disableByActNo(@PathVariable String actNo,
+                                          @Valid @RequestBody DisableAccountRequest disableAccountRequest) {
         return accountService.disableByActNo(actNo, disableAccountRequest);
     }
 }
