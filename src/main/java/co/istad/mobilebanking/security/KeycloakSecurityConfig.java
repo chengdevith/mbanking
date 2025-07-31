cd810741c15a5a8e1622afaf53e1da482ebd2c00
chengdevith <chengdevith5@gmail.com>
2025-07-31 21:36:59 +0700

    change config to oauth2 and keycloak

--- src/main/java/co/istad/mobilebanking/security/KeycloakSecurityConfig.java
+++ src/main/java/co/istad/mobilebanking/security/KeycloakSecurityConfig.java
@@ -3,24 +3,27 @@ package co.istad.mobilebanking.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class KeycloakSecurityConfig {
-
-    private final PasswordEncoder passwordEncoder;
-    private final UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain apiSecurity(HttpSecurity http) throws Exception {
@@ -30,23 +33,25 @@ public class KeycloakSecurityConfig {
         //set all request must be authenticated
         http.authorizeHttpRequests(request ->
                 request
-                        .requestMatchers(HttpMethod.GET,"/api/v1/customers/**").hasAnyRole("ADMIN","STAFF","CUSTOMER")
-                        .requestMatchers(HttpMethod.POST,"/api/v1/customers/**").hasAnyRole("ADMIN","STAFF")
-                        .requestMatchers(HttpMethod.PUT,"/api/v1/customers/**").hasAnyRole("ADMIN")
-                        .requestMatchers(HttpMethod.DELETE,"/api/v1/customers/**").hasAnyRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/v1/customers/**").hasAnyRole("ADMIN", "STAFF", "CUSTOMER")
+                        .requestMatchers(HttpMethod.POST, "/api/v1/customers/**").hasAnyRole("ADMIN", "STAFF")
+                        .requestMatchers(HttpMethod.PUT, "/api/v1/customers/**").hasAnyRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/v1/customers/**").hasAnyRole("ADMIN")
                         .requestMatchers("/api/v1/accounts/**").hasAnyRole("USER")
                         .anyRequest().authenticated()
         );
 
         // Disable firm login default
-//        http.formLogin(AbstractHttpConfigurer::disable);
+        http.formLogin(AbstractHttpConfigurer::disable);
 
         // Disable firm login default
         http.csrf(AbstractHttpConfigurer::disable);
 
         // Set security mechanism
         //Basic Authentication (username & password)
-        http.httpBasic(Customizer.withDefaults());
+        http.oauth2ResourceServer(oauth2 ->
+                oauth2.jwt(Customizer.withDefaults())
+        );
 
         // Set session Stateless
         http.sessionManagement(session ->
@@ -57,39 +62,19 @@ public class KeycloakSecurityConfig {
     }
 
     @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
-        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider(userDetailsService);
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
-        return daoAuthenticationProvider;
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+
+        Converter<Jwt, Collection<GrantedAuthority>> jwtCollectionConverter = jwt ->{
+
+            Map<String, Collection<String>> realmAccess = jwt.getClaim("realm_access");
+            Collection<String> roles = realmAccess.get("roles");
+
+            return roles.stream()
+                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                    .collect(Collectors.toList());
+        };
+        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(jwtCollectionConverter);
+        return jwtConverter;
     }
-//    @Bean
-//    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-//
-//        UserDetails admin = User
-//                .builder()
-//                .username("admin")
-//                .password("{noop}admin")
-//                .roles("ADMIN")
-//                .build();
-//        manager.createUser(admin);
-//
-//        UserDetails user = User
-//                .builder()
-//                .username("user")
-//                .password("{noop}user")
-//                .roles("USER")
-//                .build();
-//        manager.createUser(user);
-//
-//        UserDetails customer = User
-//                .builder()
-//                .username("customer")
-//                .password("{noop}customer")
-//                .roles("CUSTOMER")
-//                .build();
-//        manager.createUser(customer);
-//
-//        return manager;
-//    }
 }
--- src/main/java/co/istad/mobilebanking/security/SecurityConfig.java
+++ src/main/java/co/istad/mobilebanking/security/SecurityConfig.java
@@ -1,98 +1,98 @@
-package co.istad.mobilebanking.security;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
-public class SecurityConfig {
-
-    private final PasswordEncoder passwordEncoder;
-    private final UserDetailsService userDetailsService;
-
-    @Bean
-    public SecurityFilterChain apiSecurity(HttpSecurity http) throws Exception {
-
-        //TODO
-
-        //set all request must be authenticated
-        http.authorizeHttpRequests(request ->
-                request
-                        .requestMatchers(HttpMethod.GET,"/api/v1/customers/**").hasAnyRole("ADMIN","STAFF","CUSTOMER")
-                        .requestMatchers(HttpMethod.POST,"/api/v1/customers/**").hasAnyRole("ADMIN","STAFF")
-                        .requestMatchers(HttpMethod.PUT,"/api/v1/customers/**").hasAnyRole("ADMIN")
-                        .requestMatchers(HttpMethod.DELETE,"/api/v1/customers/**").hasAnyRole("ADMIN")
-                        .requestMatchers("/api/v1/accounts/**").hasAnyRole("USER")
-                        .anyRequest().authenticated()
-        );
-
-        // Disable firm login default
-//        http.formLogin(AbstractHttpConfigurer::disable);
-
-        // Disable firm login default
-        http.csrf(AbstractHttpConfigurer::disable);
-
-        // Set security mechanism
-        //Basic Authentication (username & password)
-        http.httpBasic(Customizer.withDefaults());
-
-        // Set session Stateless
-        http.sessionManagement(session ->
-                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        );
-
-        return http.build();
-    }
-
-    @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
-        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider(userDetailsService);
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
-        return daoAuthenticationProvider;
-    }
+//package co.istad.mobilebanking.security;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+//public class SecurityConfig {
+//
+//    private final PasswordEncoder passwordEncoder;
+//    private final UserDetailsService userDetailsService;
+//
 //    @Bean
-//    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//    public SecurityFilterChain apiSecurity(HttpSecurity http) throws Exception {
 //
-//        UserDetails admin = User
-//                .builder()
-//                .username("admin")
-//                .password("{noop}admin")
-//                .roles("ADMIN")
-//                .build();
-//        manager.createUser(admin);
+//        //TODO
 //
-//        UserDetails user = User
-//                .builder()
-//                .username("user")
-//                .password("{noop}user")
-//                .roles("USER")
-//                .build();
-//        manager.createUser(user);
+//        //set all request must be authenticated
+//        http.authorizeHttpRequests(request ->
+//                request
+//                        .requestMatchers(HttpMethod.GET,"/api/v1/customers/**").hasAnyRole("ADMIN","STAFF","CUSTOMER")
+//                        .requestMatchers(HttpMethod.POST,"/api/v1/customers/**").hasAnyRole("ADMIN","STAFF")
+//                        .requestMatchers(HttpMethod.PUT,"/api/v1/customers/**").hasAnyRole("ADMIN")
+//                        .requestMatchers(HttpMethod.DELETE,"/api/v1/customers/**").hasAnyRole("ADMIN")
+//                        .requestMatchers("/api/v1/accounts/**").hasAnyRole("USER")
+//                        .anyRequest().authenticated()
+//        );
 //
-//        UserDetails customer = User
-//                .builder()
-//                .username("customer")
-//                .password("{noop}customer")
-//                .roles("CUSTOMER")
-//                .build();
-//        manager.createUser(customer);
+//        // Disable firm login default
+////        http.formLogin(AbstractHttpConfigurer::disable);
 //
-//        return manager;
+//        // Disable firm login default
+//        http.csrf(AbstractHttpConfigurer::disable);
+//
+//        // Set security mechanism
+//        //Basic Authentication (username & password)
+//        http.httpBasic(Customizer.withDefaults());
+//
+//        // Set session Stateless
+//        http.sessionManagement(session ->
+//                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//        );
+//
+//        return http.build();
 //    }
-}
+//
+//    @Bean
+//    public DaoAuthenticationProvider daoAuthenticationProvider() {
+//        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider(userDetailsService);
+//        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+//        return daoAuthenticationProvider;
+//    }
+////    @Bean
+////    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+////        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+////
+////        UserDetails admin = User
+////                .builder()
+////                .username("admin")
+////                .password("{noop}admin")
+////                .roles("ADMIN")
+////                .build();
+////        manager.createUser(admin);
+////
+////        UserDetails user = User
+////                .builder()
+////                .username("user")
+////                .password("{noop}user")
+////                .roles("USER")
+////                .build();
+////        manager.createUser(user);
+////
+////        UserDetails customer = User
+////                .builder()
+////                .username("customer")
+////                .password("{noop}customer")
+////                .roles("CUSTOMER")
+////                .build();
+////        manager.createUser(customer);
+////
+////        return manager;
+////    }
+//}
